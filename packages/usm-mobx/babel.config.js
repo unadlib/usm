de16a0b2efe435aef9d91421b85131549f2d7480
Michael Lin <unadlib@gmail.com>
2019-02-24 04:00:49 +0800

    build(update): update babel config

--- packages/usm-mobx/babel.config.js
+++ packages/usm-mobx/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   "presets": [
-    "@babel/preset-env",
+    [
+      "@babel/preset-env", {
+        "targets": {
+          "node": "current",
+        }
+      }
+    ],
     "@babel/preset-typescript"
   ],
   "plugins": [
