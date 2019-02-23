af9b6dbdc327d9010f5abe22e82b3d4f00640570
Michael Lin <unadlib@gmail.com>
2019-02-24 04:22:28 +0800

    fix(babel): fix babel config

--- packages/usm-mobx/babel.config.js
+++ packages/usm-mobx/babel.config.js
@@ -2,9 +2,7 @@ module.exports = {
   "presets": [
     [
       "@babel/preset-env", {
-        "targets": {
-          "node": "current",
-        }
+        "useBuiltIns": 'usage',
       }
     ],
     "@babel/preset-typescript"
