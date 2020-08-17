e902a2f7952a434a7cd3b65474acfac42b506025
unadlib <unadlib@gmail.com>
2020-08-17 23:44:55 +0800

    build(setup): generating project and setup config for dev

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+declare const __DEV__: boolean;
+interface Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+}
---
+++ scripts/rollup.ts
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+import path from 'path';
+import { rollup } from 'rollup';
+import resolvePlugin from '@rollup/plugin-node-resolve';
+import replacePlugin from '@rollup/plugin-replace';
+import commonjsPlugin from '@rollup/plugin-commonjs';
+import { terser as terserPlugin } from 'rollup-plugin-terser';
+import chalk from 'chalk';
+
+type GenerateOption = {
+  inputFile: string;
+  outputFile: string;
+  format: 'cjs' | 'es' | 'umd';
+  name: string;
+  production?: boolean;
+  banner?: string;
+};
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const generateBundledModules = async ({
+  inputFile,
+  outputFile,
+  format,
+  name,
+  production = true,
+  banner,
+}: GenerateOption) => {
+  console.log(`Generating bundle:`);
+  console.log(chalk.grey(`-> ${outputFile}`));
+  const isUmd = format === 'umd';
+  const plugins = [
+    resolvePlugin(),
+    commonjsPlugin(),
+  ];
+  if (production) {
+    plugins.push(
+      replacePlugin({
+        __DEV__: isProduction ? 'false' : 'true',
+        ...(isUmd
+          ? {
+              'process.env.NODE_ENV': isProduction
+                ? "'production'"
+                : "'development'",
+            }
+          : {}),
+      }),
+      terserPlugin()
+    );
+  }
+  try {
+    const { dependencies = {}, devDependencies = {} } = require(path.resolve(
+      outputFile,
+      '../../package.json'
+    ));
+    const external = Object.keys({ ...dependencies, ...devDependencies });
+    const bundle = await rollup({
+      input: inputFile,
+      external,
+      plugins,
+    });
+    await bundle.write({
+      file: outputFile,
+      format,
+      exports: 'named',
+      name: isUmd ? name : undefined,
+      banner,
+    });
+    console.log(chalk.green(`Succeed to generate ${outputFile} bundle.\n`));
+  } catch (e) {
+    console.log(chalk.red(`Failed to generate ${outputFile} bundle.\n`));
+    console.log(chalk.red(e));
+  }
+};
+
+export { generateBundledModules };
