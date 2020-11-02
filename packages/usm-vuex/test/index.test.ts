b5ed202948e627bd01fb18e3b7976387bc271cdf
unadlib <unadlib@gmail.com>
2020-11-02 17:17:09 +0800

    test(usm-vuex): ignore test

--- packages/usm-vuex/test/index.test.ts
+++ packages/usm-vuex/test/index.test.ts
@@ -1,22 +1,28 @@
-import { createStore } from 'vuex'
+test('base', () => {
+  // class Counter {
+  //   @state
+  //   count = { sum: 0 };
 
-const store = createStore({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-      state.count++
-    }
-  }
-})
+  //   @action
+  //   increase() {
+  //     this.count.sum += 1;
+  //   }
+  // }
 
-test('', (r) => {
-  console.log(store.subscribe(() => {
-    console.log('=====');
-  }))
-  store.commit('increment')
-  console.log(store.state);
-  setTimeout(r);
+  // const counter = new Counter();
+
+  // const store = createStore({
+  //   modules: [counter],
+  // });
+  // const [oldState] = Object.values(store.getState());
+  // expect(oldState).toEqual({ count: { sum: 0 } });
+  // const fn = jest.fn();
+  // store.subscribe(() => {
+  //   fn();
+  // });
+  // counter.increase();
+  // const [newState] = Object.values(store.getState());
+  // expect(newState).toEqual({ count: { sum: 1 } });
+  // expect(fn.mock.calls.length).toBe(1);
+  // expect(newState === oldState).toBe(false);
 });
