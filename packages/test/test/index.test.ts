c929012a346e2415dc8f2dab099d334a932b5029
unadlib <unadlib@gmail.com>
2020-11-06 23:19:52 +0800

    fix(test): fix testing type

--- packages/test/test/index.test.ts
+++ packages/test/test/index.test.ts
@@ -3,11 +3,13 @@ import * as usmMobx from 'usm-mobx';
 import * as usmRedux from 'usm-redux';
 import * as usmVuex from 'usm-vuex';
 
-const packages = {usm, usmMobx, usmVuex, usmRedux};
+const packages = { usm, usmMobx, usmVuex, usmRedux };
 
 test('base', () => {
   for (const key in packages) {
-    const { createStore, action, state } = packages[key as keyof typeof packages];
+    const { createStore, action, state } = packages[
+      key as keyof typeof packages
+    ];
     class Counter {
       @state
       count = { sum: 0 };
@@ -32,6 +34,41 @@ test('base', () => {
     });
     counter.increase();
     const [newState] = Object.values(store.getState());
+    expect(newState.count).toEqual({ sum: 1 });
+    expect(fn.mock.calls.length).toBe(1);
+  }
+});
+
+test('base', () => {
+  for (const key in packages) {
+    const { createStore, action, state } = packages[
+      key as keyof typeof packages
+    ];
+    class Counter {
+      @state
+      count = { sum: 0 };
+
+      @action
+      increase() {
+        this.count.sum += 1;
+      }
+    }
+
+    const counter = new Counter();
+
+    const store = createStore({
+      modules: [counter],
+      strict: true,
+    });
+
+    const [oldState] = Object.values<{ count: Counter }>(store.getState());
+    expect(oldState.count).toEqual({ sum: 0 });
+    const fn = jest.fn();
+    store.subscribe(() => {
+      fn();
+    });
+    counter.increase();
+    const [newState] = Object.values<{ count: Counter }>(store.getState());
     expect(newState.count).toEqual({ sum: 1 });
     expect(fn.mock.calls.length).toBe(1);
   }
