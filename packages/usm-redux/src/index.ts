e393c94ae50ee4e25e0a3eca89e1b6b00432fd49
unadlib <unadlib@gmail.com>
2021-03-29 01:53:41 +0800

    fix(enablepatches): export enablePatches

--- packages/usm-redux/src/index.ts
+++ packages/usm-redux/src/index.ts
@@ -2,9 +2,10 @@ export { createStore, setPatchesToggle } from './createStore';
 export { subscribe, watch } from './subscribe';
 export { action, computed, state } from './decorators/index';
 export { getStagedState } from './utils/index';
+export { enablePatches } from 'immer';
 export * from './interface';
 export * from './constant';
 
 /**
- * Remove: export getStagedState, setPatchesToggle and constant.
+ * Remove: export enablePatches, getStagedState, setPatchesToggle and constant.
  */
