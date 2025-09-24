3decb2a0843f5d74e39e20b5d890de79e0d37941
unadlib <unadlib@gmail.com>
2025-09-24 23:53:32 +0800

    revert(usm): update

--- packages/usm-pinia/src/decorators/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './action';
-export * from './computed';
-export * from './state';
--- packages/usm-pinia/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { createStore } from './createStore';
-export { subscribe, watch } from './subscribe';
-export { action, computed, state } from './decorators/index';
-export * from './interface';
-export * from './constant';
